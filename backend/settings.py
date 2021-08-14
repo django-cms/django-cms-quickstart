0f846efcc17b8c55de9c19ce6d079c17a51dd51a
Mario Colombo <mario@what.digital>
2021-08-14 18:01:48 +0200

    add static files for whitenoise demo

--- backend/settings.py
+++ backend/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
