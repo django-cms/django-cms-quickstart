17f2aff609a8487207072ba8e3196da5645cbdbe
Mario Colombo <mario@what.digital>
2021-08-14 18:23:18 +0200

    fix collectstatic best practises

--- backend/settings.py
+++ backend/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
