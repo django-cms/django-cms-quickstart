0ed227506083565768e89f399b4767b9219cfd16
Mario Colombo <mario@what.digital>
2024-11-01 10:24:05 +0100

    improve testing setup for djangocms-link

--- backend/settings.py
+++ backend/settings.py
@@ -58,8 +58,8 @@ INSTALLED_APPS = [
     'parler',
 
     # links
-    'djangocms-link',
-    
+    'djangocms_link',
+
     # the next-gen text editor - optional, but used in most projects
     'djangocms_text',
 
@@ -81,6 +81,12 @@ INSTALLED_APPS = [
     'djangocms_frontend.contrib.image',
     'djangocms_frontend.contrib.tabs',
     'djangocms_frontend.contrib.utilities',
+
+    'taggit',
+    'taggit_autosuggest',
+    'meta',
+    'sortedm2m',
+    'djangocms_blog',
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('taggit_autosuggest/', include('taggit_autosuggest.urls')),
+
 ]
 
 if settings.DEBUG:
