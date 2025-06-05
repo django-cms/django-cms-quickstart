dcd87cb6c83181418659634d8ca1d4049b5324ef
Fabian Braun <fsbraun@gmx.de>
2025-06-05 15:23:33 +0200

    feat: Switch to djangocms-simple-admin-style

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'backend',
 
     # optional, but used in most projects
-    'djangocms_admin_style',
+    'djangocms_simple_admin_style',
 
     'django.contrib.admin',
     'django.contrib.auth',
