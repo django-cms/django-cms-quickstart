7c7c241957aeba2037a39a8d6ac145d647ea45d4
dennisaguilar1989@gmail.com <dennisaguilar1989@gmail.com>
2025-06-08 09:03:41 -0500

    renamed spec to login.cy.js. Edited home to include test for base functionality. Extracted login into beforeEach.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,17 +1,25 @@
-it('Assert Home Page', () => {
+beforeEach(() => {
     const userName = Cypress.env("USERNAME");
     const password = Cypress.env("PASSWORD");
 
     cy.visit('/');
     cy.login(userName, password);
+})
 
+it('Home Page renders properly', () => {
     cy.get('.cms-toolbar-item-logo')
     .should('have.text','django CMS');
-    
+
+    //Static content verification
     cy.get('.cms-welcome-heading')
     .invoke('text')
     .should((text) => {
         const trimmedText = String(text).trim();
         expect(trimmedText).to.equal('Welcome to your new django CMS installation!');
     });
+});
+
+it('Home Page interactions work', () => {
+    cy.contains('example.com')
+        .should('exist');
 });
