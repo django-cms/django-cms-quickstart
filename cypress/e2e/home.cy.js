edf750e4aa15472d77c17c3e47be8a61478381d8
dennisaguilar1989@gmail.com <dennisaguilar1989@gmail.com>
2025-06-08 08:16:36 -0500

    Added assertion to Home header. Trimmed leading and trailing whitespace

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -7,5 +7,11 @@ it('Assert Home Page', () => {
 
     cy.get('.cms-toolbar-item-logo')
     .should('have.text','django CMS');
-
+    
+    cy.get('.cms-welcome-heading')
+    .invoke('text')
+    .should((text) => {
+        const trimmedText = String(text).trim();
+        expect(trimmedText).to.equal('Welcome to your new django CMS installation!');
+    });
 });
